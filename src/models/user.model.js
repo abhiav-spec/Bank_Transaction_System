81105b579db58ca948db53399c6a56fba0d8ffa4
abhiav-spec <kumarabhinav6649@gmail.com>
2026-03-12 18:26:08 +0530

    project started 11 mar 2026

---
+++ src/models/user.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+   email:{
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+    lowercase: true,
+    match: [/^.+@(?:[\w-]+\.)+\w+$/, "Please enter a valid email address"],
+    unique: [true, "Email already exists"],
+},
+
+name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+},
+
+password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters long"],
+    select: false
+}
+
+},{
+    timestamps: true
+});
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) {
+        return next();
+    }
+
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+
+    next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
