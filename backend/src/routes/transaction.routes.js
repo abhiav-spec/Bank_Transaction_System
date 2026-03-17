a6c9d1a12110d8687f9ba57ac08f5838328af7df
abhiav-spec <kumarabhinav6649@gmail.com>
2026-03-17 15:39:24 +0530

    Fix case-sensitive initial funds controller import for Linux

--- backend/src/routes/transaction.routes.js
+++ backend/src/routes/transaction.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const { createTransactionController, getTransactionsController } = require('../controllers/transaction.controller');
 const { authMiddleware, systemUserMiddleware } = require('../middleware/auth.middleware');
-const { createInitialFundsController } = require('../controllers/initialfunds.controller');
+const { createInitialFundsController } = require('../controllers/Initialfunds.controller');
 
 router.get('/', authMiddleware, getTransactionsController);
 router.post('/', authMiddleware, createTransactionController);
